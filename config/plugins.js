488d87806b84928cdfa0ee3e4688f3bba3c07097
Mateo Olarte <mateo.olarte@dowjones.com>
2023-12-28 14:42:59 -0500

    add validation to only upload to s3 when in prod env

--- config/plugins.js
+++ config/plugins.js
@@ -1,22 +1,28 @@
-module.exports = ({ env }) => ({
-  upload: {
-    config: {
-      provider: "aws-s3",
-      providerOptions: {
-        accessKeyId: env("AWS_ACCESS_KEY_ID"),
-        secretAccessKey: env("AWS_ACCESS_SECRET"),
-        region: env("AWS_REGION"),
-        params: {
-          ACL: env("AWS_ACL", "public-read"),
-          signedUrlExpires: env("AWS_SIGNED_URL_EXPIRES", 15 * 60),
-          Bucket: env("AWS_BUCKET"),
+module.exports = ({ env }) => {
+  const isDevelopment = env("NODE_ENV") === "development";
+
+  if (isDevelopment) return {};
+
+  return {
+    upload: {
+      config: {
+        provider: "aws-s3",
+        providerOptions: {
+          accessKeyId: env("AWS_ACCESS_KEY_ID"),
+          secretAccessKey: env("AWS_ACCESS_SECRET"),
+          region: env("AWS_REGION"),
+          params: {
+            ACL: env("AWS_ACL", "public-read"),
+            signedUrlExpires: env("AWS_SIGNED_URL_EXPIRES", 15 * 60),
+            Bucket: env("AWS_BUCKET"),
+          },
+        },
+        actionOptions: {
+          upload: {},
+          uploadStream: {},
+          delete: {},
         },
       },
-      actionOptions: {
-        upload: {},
-        uploadStream: {},
-        delete: {},
-      },
     },
-  },
-});
+  };
+};
